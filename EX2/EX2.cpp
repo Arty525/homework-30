71b9e970749933d488d97f18536da40659678509
Andrey <89473462+Arty525@users.noreply.github.com>
2022-12-11 13:17:56 +0300

    Add files via upload

--- EX2/EX2.cpp
+++ EX2/EX2.cpp
@@ -1,1 +1,16 @@
+﻿#include <cpr/cpr.h>
+#include <iostream>
+#include <string>
 
+int main() {
+	std::string header;
+	cpr::Response r = cpr::Get(cpr::Url("http://httpbin.org/html"), cpr::Header{ {"Accept", "text/html"} });
+
+	int start = r.text.find("<h1>", 0) + 4;
+	int end = r.text.find("</h1>", start);
+
+	header = r.text.substr(start, end - start);
+	std::cout << "Header: " << header << std::endl;
+
+	return 0;
+}
